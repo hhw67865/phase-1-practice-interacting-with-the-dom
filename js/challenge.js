67adb416f7797eb8ad2d22c88690dccba9a6b61a
Henry Wu <hhw67865@gmail.com>
2022-10-14 11:18:11 -0400

    test

--- js/challenge.js
+++ js/challenge.js
@@ -1,7 +1,7 @@
 const plus = document.querySelector("#plus");
 const counter = document.querySelector("#counter");
 plus.addEventListener("click", (e) => {
-const counterValue = counter.textContent 
-const counterInt = parseInt(counterValue, 10) + 1;
-counter.textContent = counterInt;
-})
+    const counterValue = counter.textContent 
+    const counterInt = parseInt(counterValue, 10) + 1;
+    counter.textContent = counterInt;
+});
