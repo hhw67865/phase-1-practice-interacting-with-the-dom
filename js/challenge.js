d99f37ad1eaf7c5e6b54f6e2129f6e163f2fc042
Nick Landas <nick.landas@gmail.com>
2022-10-14 10:13:04 -0500

    Added plus

--- js/challenge.js
+++ js/challenge.js
@@ -1,1 +1,7 @@
-console.log("test");
+const plus = document.querySelector("#plus");
+const counter = document.querySelector("#counter");
+plus.addEventListener("click", (e) => {
+const counterValue = counter.textContent 
+const counterInt = parseInt(counterValue, 10) + 1;
+counter.textContent = counterInt;
+})
